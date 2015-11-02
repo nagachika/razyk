a42c1afbdb0722efc86a38a8188375d547725e14
nagachika <nagachika@ruby-lang.org>
2015-11-03 00:54:50 +0900

    remove unused API in webapp mode.

--- lib/razyk/webapp.rb
+++ lib/razyk/webapp.rb
@@ -5,8 +5,6 @@ require "thread"
 require "stringio"
 require "json"
 
-require "razyk/graph"
-
 module RazyK
   class WebApp
     class InputStream
@@ -34,26 +32,11 @@ module RazyK
       end
     end
 
-    def initialize
-      reset
-    end
-
-    def reset
-      @vm = nil
-      @graph = 0
-      @step = 0
-      @thread = nil
-      @port_in = StringIO.new
-      @port_out = StringIO.new
-      @queue = Queue.new
-    end
-
     def template(name)
       File.join(File.dirname(File.expand_path(__FILE__)), "webapp", "templates", name)
     end
 
     def main_page(req)
-      reset
       res = Rack::Response.new
       res.status = 200
       res.write File.read(template("main.html"))
@@ -67,99 +50,6 @@ module RazyK
         <html><head><title>#{req.path} is not found</title></head>
         <body>#{req.path} is not found</body></html>
       EOF
-      res
-    end
-
-    def reduce_thread(vm)
-      while q = @queue.pop
-        begin
-          q.push(vm.reduce)
-        rescue
-          q.push($!)
-        end
-      end
-    end
-
-    def set_program(req)
-      res = Rack::Response.new
-      begin
-        tree = RazyK::Parser.parse(req.params["program"])
-        if req.params["mode"] == "true"
-          root = Pair.new(:OUT, Pair.new(tree, :IN))
-          @port_in = StringIO.new(req.params["stdin"] || "")
-          recursive = false
-        else
-          root = tree
-          recursive = true
-        end
-        # discard previous vm and thread
-        if @thread
-          @thread.kill
-          @thread = nil
-        end
-        @vm = VM.new(root, @port_in, @port_out, recursive)
-        # start Thread for reduction
-        @thread = Thread.start do reduce_thread(@vm) end
-      rescue
-        res.status = 501
-        puts $!.message, $@
-        return res
-      end
-      res.header["Content-Type"] = "text/json"
-      res.write('{"status":"success"}')
-      res
-    end
-
-    def step(req)
-      res = Rack::Response.new
-      res.header["Content-Type"] = "text/plain"
-      if @thread
-        rep = Queue.new
-        @queue.push(rep)
-        ret = rep.pop
-        if ret.is_a?(Exception)
-          raise ret
-        end
-        if ret
-          @step += 1
-        end
-        res.write("OK")
-      else
-        res.write("enter program first")
-      end
-      res
-    end
-
-    def stdout(req)
-      res = Rack::Response.new
-      res.header["Content-Type"] = "text/plain"
-      res.write(@port_out.string.inspect)
-      res
-    end
-
-    def expression(req)
-      res = Rack::Response.new
-      res.header["Content-Type"] = "text/plain"
-      if @vm
-        res.write(@vm.tree.inspect)
-      else
-        res.write("enter program first")
-      end
-      res
-    end
-
-    def graph(req)
-      res = Rack::Response.new
-      if @vm
-        res.header["Content-Type"] = "image/svg+xml"
-        tmpfile = Tempfile.new("razyk_graph")
-        RazyK::Graph.graph(@vm.tree, :style => :dag).output(:svg => tmpfile.path)
-        res.write(tmpfile.read)
-        tmpfile.unlink
-      else
-        res.header["Content-Type"] = "text/plain"
-        res.write("enter program first")
-      end
       res
     end
 
@@ -192,16 +82,6 @@ module RazyK
       case req.path
       when "/"
         res = main_page(req)
-      when "/set_program"
-        res = set_program(req)
-      when "/step"
-        res = step(req)
-      when "/stdout"
-        res = stdout(req)
-      when "/expression"
-        res = expression(req)
-      when "/graph"
-        res = graph(req)
       when "/reduce"
         res = reduce(req)
       else
